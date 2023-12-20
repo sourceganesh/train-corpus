a00e847cc8861445a3ac62e9fb811387be359fc0
Ganesh Saravanan <57284555+sourceganesh@users.noreply.github.com>
2023-12-20 18:00:23 +0530

    Create train_data.py

---
+++ train_data.py
@@ -0,0 +1,37 @@
+training_data = [
+    ["software_development", "coding", "version_control", "software_testing", "algorithm_design", "debugging", "code_refactoring", "unit_testing", "web_development", "frameworks", "devops", "microservices", "agile_development", "ci_cd", "code_review", "pair_programming"],
+    ["data_science", "statistical_analysis", "data_cleaning", "machine_learning_algorithms", "data_mining", "data_wrangling", "time_series_analysis", "feature_selection", "data_visualization_tools", "big_data_frameworks", "apache_spark", "jupyter_notebooks", "predictive_modeling", "clustering", "feature_engineering", "natural_language_processing", "ensemble_learning"],
+    ["cybersecurity", "penetration_testing", "network_security", "incident_response", "cryptography", "security_auditing", "threat_hunting", "malware_analysis", "firewalls", "intrusion_detection_systems", "vpns", "endpoint_protection", "siem", "threat_intelligence", "vulnerability_assessment", "security_awareness_training", "red_team_vs_blue_team"],
+    ["marketing", "seo", "social_media_management", "content_creation", "branding", "marketing_analytics", "influencer_marketing", "conversion_rate_optimization", "analytics_platforms", "social_media_tools", "marketing_automation", "crm_integration", "a_b_testing", "customer_segmentation", "customer_journey_mapping", "brand_strategy"],
+    ["project_management", "scope_management", "time_management", "risk_management", "stakeholder_communication", "conflict_resolution", "decision_making", "project_management_software", "collaboration_tools", "gantt_charts", "kanban_boards", "agile", "scrum", "kanban", "critical_path_method", "earned_value_management", "project_portfolio_management"],
+    ["cloud_computing", "cloud_architecture", "virtualization", "containerization", "cloud_security", "serverless_computing", "infrastructure_as_code", "aws", "azure", "google_cloud", "docker", "kubernetes", "terraform", "serverless_framework", "scalability", "high_availability", "cost_optimization", "cloud_migration_strategies"],
+    ["healthcare", "electronic_health_records", "telemedicine", "medical_coding", "healthcare_analytics", "health_informatics", "health_data_privacy", "health_information_exchange", "pacs", "telehealth_platforms", "clinical_decision_support_systems", "health_data_interoperability", "patient_engagement_strategies"],
+    ["data_analytics", "exploratory_data_analysis", "statistical_analysis", "data_cleaning", "data_visualization", "data_interpretation", "predictive_modeling", "sql", "excel", "bi_tools", "python_data_libraries", "regression_analysis", "time_series_forecasting", "hypothesis_testing", "bayesian_statistics"],
+    ["business_intelligence", "data_warehousing", "olap", "data_mining", "dashboard_design", "data_governance", "self_service_bi", "businessobjects", "tableau", "qlikview", "power_bi", "kpi_analysis", "reporting_automation", "predictive_analytics", "balanced_scorecard"],
+    ["finance", "financial_modeling", "risk_management", "investment_analysis", "portfolio_optimization", "financial_reporting", "algorithmic_trading", "bloomberg_terminal", "excel", "python_for_finance", "trading_platforms", "asset_allocation", "derivatives_trading", "financial_forecasting", "monte_carlo_simulation"],
+    ["database_management", "database_design", "query_optimization", "data_modeling", "database_administration", "indexing", "performance_tuning", "mysql", "postgresql", "mongodb", "oracle", "sql_server", "cassandra", "normalization", "backup_and_recovery", "database_security", "nosql_database_management"],
+    ["operations", "supply_chain_management", "logistics", "quality_control", "process_improvement", "inventory_optimization", "lean_six_sigma", "erp_systems", "inventory_management_software", "supply_chain_analytics", "rfid_technology", "demand_forecasting", "jit_inventory", "vendor_management", "continuous_improvement"],
+    ["banking", "risk_assessment", "financial_compliance", "credit_analysis", "fraud_detection", "banking_regulations", "fintech", "core_banking_systems", "blockchain_in_banking", "mobile_banking_applications", "aml", "credit_scoring_models", "financial_fraud_prevention", "regulatory_compliance"],
+    ["management", "leadership", "decision_making", "strategic_planning", "change_management", "team_building", "management_software", "performance_analytics", "leadership_development", "swot_analysis", "organizational_development", "conflict_resolution"],
+    ["product_management", "product_development", "roadmapping", "user_feedback_analysis", "market_research",    "competitive_analysis", "product_management_tools", "agile_product_management", "product_lifecycle_management", "mvp", "product_launch_strategies", "user_persona_development"],
+    
+    ["sales", "relationship_building", "sales_strategy", "negotiation", "sales_forecasting", "crm_systems", "sales_analytics_tools", "sales_automation", "consultative_selling", "value_based_selling", "sales_funnel_management"],
+    
+    ["human_resources", "recruitment", "employee_relations", "hr_analytics", "talent_management", "workplace_diversity", "hr_management_systems", "applicant_tracking_systems", "performance_management", "compensation_planning", "employee_engagement"],
+    
+    ["database_administration", "database_security", "backup_and_recovery", "performance_tuning", "database_monitoring", "oracle_db", "sql_server", "mysql", "database_auditing", "data_archiving", "capacity_planning", "database_upgrades"],
+    
+    ["supply_chain_management", "demand_planning", "inventory_management", "supplier_relationship_management", "supply_chain_analytics", "erp_systems", "rfid_technology", "jit_inventory", "logistics_optimization", "distribution_network_design"],
+    
+    ["e_commerce", "online_merchandising", "conversion_rate_optimization", "ecommerce_analytics", "ecommerce_platforms", "payment_gateways", "shopping_cart_software", "customer_retention_strategies", "personalization", "user_experience_design"],
+    
+    ["network_security", "firewall_configuration", "intrusion_detection", "vpn_implementation", "network_forensics", "cisco_security_appliances", "ids_ips_systems", "siem", "network_scanning", "security_protocols", "access_control"],
+    
+    ["application_security", "secure_coding", "penetration_testing", "code_review", "web_application_firewall", "owasp_top_ten", "security_testing_tools", "threat_modeling", "secure_sdlc", "api_security"],
+    
+    ["cloud_security", "iam", "encryption", "security_groups", "cloud_security_platforms", "casb_solutions", "security_compliance", "incident_response_in_the_cloud"],
+    
+    ["incident_response", "cyber_threat_intelligence", "incident_handling", "digital_forensics", "siem", "endpoint_detection_response", "cyber_incident_triage", "root_cause_analysis", "cybersecurity_incident_playbooks"],
+    
+    ["cybersecurity_governance", "risk_management", "security_policies", "compliance_frameworks", "grc_platforms", "security_audits", "security_awareness_training", "security_metrics"]
+]
